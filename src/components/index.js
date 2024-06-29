63d635a12e0e1bb0aa3f18ee685dd92b1c6915e0
Naveen Kumar <naveenjangid178@gmail.com>
2024-06-29 11:01:37 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Article from "./article/Article";
+import Brand from "./brand/Brand";
+import CTA from "./cta/CTA";
+import Feature from "./feature/Feature";
+import Navbar from "./navbar/Navbar";
+
+export {
+    Article,
+    Brand,
+    CTA,
+    Feature,
+    Navbar
+}
